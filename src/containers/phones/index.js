d0fde3df33fa60af93ba59e11b24e81ae0ecefb5
endorphin82 <endorphin82@gmail.com>
2019-01-05 04:01:14 +0200

    002-redux-devtools-extension

---
+++ src/containers/phones/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Phones = () => {
+  return (
+    <div>
+phones
+    </div>
+  );
+};
+
+export default Phones;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-import './main.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import reducers from "./reducers";
+import { Provider } from "react-redux";
 
-document.write('Hello React/Redux!')
+import Layout from "./containers/layout";
+import Phones from "./containers/phones";
+
+import "./main.css";
+
+const store = createStore(
+  reducers,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Layout>
+        <Route path='/' component={Phones} />
+      </Layout>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
