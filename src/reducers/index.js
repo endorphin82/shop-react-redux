ee22dfd26cf7dc9d401e001be4f357ee1dd2ef80
endorphin82 <endorphin82@gmail.com>
2019-01-07 01:28:12 +0200

    004-ramda-reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { routerReducer } from "react-router-redux";
+import phonesPage from "./phonesPage";
 
 import phones from "./phones";
 
 export default combineReducers({
   routing: routerReducer,
-  phones
+  phones,
+  phonesPage
 });
